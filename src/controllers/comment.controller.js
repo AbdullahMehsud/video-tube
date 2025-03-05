d663ce237df4c66f8a1a723b6ce76791244094e2
abdullah mehsud <abdullahmsd85@gmail.com>
2025-03-06 00:00:26 +0500

    comment controller and routes

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,13 +8,30 @@ const getVideoComment = asyncHandler (async(req, res) => {
     //TODO: Get all video comments
     const {videoId} = req.params
     const {page =1, limit= 10} = req.query
+
+    if(!mongoose.Types.ObjectId.isValid(videoId)){
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    try {
+        const comment = await Comment.find({ video: videoId})
+        .populate("owner", "username avatar")
+        .sort({createdAt: -1})
+        .skip((page - 1)*limit)
+        .limit(parseInt(limit))
+
+        return res.status(200).json(new apiResponse(201, comment, "All video comments fetched successfully"))
+    } catch (error) {
+        console.log("error while fetching video comments", error);
+        throw new ApiError(500, "Some thing went wrong while fetching video comments")
+        
+    }
 })
 
 const addComment = asyncHandler( async( req, res) => {
     //TODO: add comment to the video
     const {content} = req.body
     const {videoId} = req.params
-    const userId = req.user?._id
     if(content?.trim() === "") {
         throw new ApiError(401, "Content is required")
     }
@@ -31,7 +48,7 @@ const addComment = asyncHandler( async( req, res) => {
             video: videoId
         })
 
-        return res.status(200).json( new apiResponse( 201, "comment added successfully"))
+        return res.status(200).json( new apiResponse( 201, comment, "comment added successfully"))
     } catch (error) {
         console.log("error while commenting", error);
         throw new ApiError(500, "some thing went wrong while comminting")
@@ -41,10 +58,49 @@ const addComment = asyncHandler( async( req, res) => {
 
 const updateComment = asyncHandler( async( req, res) => {
     //TODO: update comment
+    const {commentId} = req.params
+    const {content} = req.body
+    
+    if(!mongoose.Types.ObjectId.isValid(commentId)){
+        throw new ApiError(400, "comment ID required")
+    }
+
+    if(!content?.trim() === ""){
+        throw new ApiError(400, "content can't be empty")
+    }
+
+   try {
+     const comment = await Comment.findOneAndUpdate(
+        {_id: commentId, owner: req.user?._id},
+        {$set: {content: content}},
+        {new: true}
+     )
+     console.log("comment:" ,comment);
+     
+     if (!comment){
+         throw new ApiError(404, "Comment not found")
+     }
+ 
+     return res.status(200).json( new apiResponse(201, comment, "Comment updated successfully"))
+   } catch (error) {
+    console.log("error while updating comment", error);
+    throw new ApiError(500, "Some thing went wrong while updating comment")
+   }
+    
 })
 
 const deleteComment = asyncHandler( async( req, res ) => {
     //TODO: delete comment of the vidoe
+    const {commentId} = req.params
+    const comment = await Comment.findByIdAndDelete(
+        {_id: commentId, owner: req.user?._id}
+    )
+
+    if(!comment){
+        throw new ApiError(500, "Comment not Found")
+    }
+
+    return res.status(200).json( new apiResponse(201, comment, "Comment deleted successfully"))
 })
 
 export {
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { addComment } from "../controllers/comment.controller.js";
+import {
+        addComment, 
+        deleteComment, 
+        getVideoComment, 
+        updateComment 
+        } from "../controllers/comment.controller.js";
 import { verifyJwt } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
 
 router.route("/:videoId/comment").post(verifyJwt, addComment)
-
+router.route("/:commentId/update-comment").patch(verifyJwt, updateComment)
+router.route("/:commentId/delete-comment").delete(verifyJwt, deleteComment)
+router.route("/:videoId/get-video-comment").get(verifyJwt, getVideoComment)
 export default router
